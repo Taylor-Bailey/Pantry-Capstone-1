dfab5e458fd2079cd59ecf66e4eb9b2a9c8ad382
Taylor Bailey <taylor.williams.bailey@gmail.com>
2018-03-19 14:32:08 -0500

    got user in console log and fixed app js in index

--- js/main.js
+++ js/main.js
@@ -2,3 +2,11 @@
 let config = require("./config");
 let users = require("./users");
 let fbKey = require("./fb-key");
+let $ = require('../lib/node_modules/jquery');
+
+$("#loginButton").click(function(){
+    users.logInGoogle()
+    .then((login) => {
+        console.log("got the user", login.user.uid);
+    });
+});
