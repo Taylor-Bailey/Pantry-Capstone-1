9ddfcc82888adfbf26814796594b57fc5f34618a
Taylor Bailey <taylor.williams.bailey@gmail.com>
2018-03-23 13:41:58 -0500

    got api key and created api key object

---
+++ js/api-key.js
@@ -0,0 +1,12 @@
+"use strict";
+
+console.log("api-keys.js running");
+
+let spoonKey = {
+    apiKey: "bqzDK8qj6omshBEa1dX9QWrsvLnKp1ymN8Kjsn3jsOuBpaXscp",
+    authDomain: "https://spoonacular-recipe-food-nutrition-v1.p.mashape.com",
+    ingredients: "/recipes/findByIngredients",
+    recipeInfo: "/recipes/informationBulk"
+  };
+
+  module.exports = spoonKey;
